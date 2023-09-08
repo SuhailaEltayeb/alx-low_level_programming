1e6e39b69cca2d6bb2e67aa8257ec41d7c26486e
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2023-09-08 16:00:22 -0700

    lowercase alphabet 1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program that prints all single digit numbers of base 10
+ *
+ * Return: Always return 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+
+		printf("%d", i);
+
+	}
+	putchar ('\n');
+	return (0);
+}
