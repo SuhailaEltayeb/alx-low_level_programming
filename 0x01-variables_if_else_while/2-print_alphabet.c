87e0d1d8c936dce728267f7b3930fa2e3c029b51
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2023-09-08 14:54:39 -0700

    printing alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * DescriptionL A c program that prints alphabet
+ *
+ * Return: Always return 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar (ch)
+
