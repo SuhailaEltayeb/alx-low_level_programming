99f7a1ed0be50601e811f543db0aec498da3d4a2
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2023-09-09 13:54:57 -0700

    correction task 8 specific qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
+#include <stdio.h>
 
 /**
- * main - entry point
+ * main - program that prints specific text.
  *
- * Description: program that prints exactly specific text.
- *
- * Return: always 0 (success)
+ * Return: always return 0 (success)
 */
-int main(viod) 
+
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	return (0);
 }
