4d55bee67f4223bb5a3510076de490414f12f99a
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2023-09-09 13:58:32 -0700

    correction task 8 specific qoute 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 /**
  * main - program that prints specific text.
  *
- * Return: always return 0 (success)
+ * Return: always return 1 (success)
 */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
