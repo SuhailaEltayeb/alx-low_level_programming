c77bf1fe607b36ef659c203aae893b3aca4d0333
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2023-09-12 18:53:48 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,6 +8,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0 ; i <= n ; i++)
@@ -15,7 +16,7 @@ void print_times_table(int n)
 			for (j = 0 ; j <= n ; j++)
 			{
 				k = i * j;
-				if (j ==0)
+				if (j == 0)
 				{
 					_putchar(k + '0');
 				}
@@ -40,7 +41,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10 ) + '0');
+					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 			}
