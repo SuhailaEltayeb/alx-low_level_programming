556405353bd804701c200987478980ce92516a4a
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2023-09-12 18:23:23 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,13 +11,13 @@ int print_last_digit(int n)
 	int a;
 
 	if (n < 0)
-	{
 		n = -n;
 
 		a = n % 10;
 
-		if (a <0)
+		if (a < 0)
 			a = -a;
-		-putchar(a + '0);
+		_putchar(a + '0');
+		
 		return (a);
-	}
+}
