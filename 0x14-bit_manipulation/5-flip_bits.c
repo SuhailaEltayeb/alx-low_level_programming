5ccc9b8e16f55647a85f48a45c3103002b9524d5
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2023-10-26 12:57:41 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits- function to return #of bits to flip
+ * @n: input number 1
+ * @m: input number 2
+ * Return: # of bits to be shifted
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int c;
+	unsigned int r;
+
+	r = n ^ m;
+	for (c = 0; r > 0;)
+	{
+		if ((r & 1) == 1)
+			c++;
+		r = r >> 1;
+	}
+	return (c);
+}
