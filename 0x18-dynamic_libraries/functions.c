4b50927c54e7836fb5f4238a7f0ff70ee4f52249
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2023-12-12 10:05:57 -0800

    101-make_me_win.sh

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - function to add 2 numbers
+ * @x: 1st number
+ * @y: 2nd number
+ * Return: sum of x and y
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+/**
+ * sub - function to subtract 2 numbers
+ * @x: 1st number
+ * @y: 2nd number
+ * Return: subtraction of x and y
+ */
+int sub(int x, int y)
+{
+	return (x - y);
+}
+/**
+ * mul - function to multiply 2 numbers
+ * @x: 1st number
+ * @y: 2nd number
+ * Return: multiplicaation of x and y
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
+/**
+ * mod - function to return modulus of 2 numbers
+ * @x: 1st number
+ * @y: 2nd number
+ * Return: modulus of x and y
+ */
+int mod(int x, int y)
+{
+	return (x % y);
+}
+/**
+ * div - function to divide 2 numbers
+ * @x: 1st number
+ * @y: 2nd number
+ * Return: division of x and y
+ */
+int div(int x, int y)
+{
+	return (x / y);
+}
