9f153118c5427ed486180b9a5e2359b9d7ef2d07
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2023-12-22 13:52:03 -0800

    5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - fuction to print values in hash table
+ * @ht: hash table to be printed
+ *
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp;
+	char first = 0;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("(");
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			if (first == 1)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			first = 1;
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
+}
