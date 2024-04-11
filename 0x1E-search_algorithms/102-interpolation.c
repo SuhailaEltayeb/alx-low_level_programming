7f3e2924f06a0d5d9a2955073e07c9c5f041885a
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-04-11 15:27:15 -0700

    102-interpolation

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,7 +1,8 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - search for value in sorted ints array by Interpolation search
+ * interpolation_search - search 4 val in sorted ints
+ * array by Interpolation search.
  * @array: pointer to the first element of the array to search in
  * @size: is the number of elements in array
  * @value: the value to search for
@@ -11,16 +12,17 @@
 int interpolation_search(int *array, size_t size, int value)
 {
 	size_t right, left, probe_position;
-	
+
 	left = 0;
 	right = size - 1;
 	if (array == NULL || size == 0)
 		return (-1);
 	while (left <= right && value >= array[left] && value <= array[right])
 	{
-	        probe_position = left + (((double)(right - left) /
+		probe_position = left + (((double)(right - left) /
 			(array[right] - array[left])) * (value - array[left]));
-		printf("Value checked array[%d] = [%d]\n", (int)probe_position, array[probe_position]);
+		printf("Value checked array[%d] = [%d]\n",
+			(int)probe_position, array[probe_position]);
 
 		if (array[probe_position] == value)
 			return (probe_position);
@@ -34,6 +36,7 @@ int interpolation_search(int *array, size_t size, int value)
 		printf(" is out of range\n");
 	else
 		printf(" = [%d]\n", array[left]);
+
 	return (-1);
 }
 
